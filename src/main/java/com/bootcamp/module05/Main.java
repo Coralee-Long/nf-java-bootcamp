13bb2193cadd3f6077aef5f0e053cc821fcb0193
Coralee Long <coralee@coralees-mbp.localdomain>
2024-11-20 10:09:20 +0100

    update

--- src/main/java/com/bootcamp/module05/Main.java
+++ src/main/java/com/bootcamp/module05/Main.java
@@ -1,4 +1,17 @@
 package com.bootcamp.module05;
 
+import com.bootcamp.module01.Exercises;
+import com.bootcamp.module01.Lecture;
+
 public class Main {
+    public static void main(String[] args) {
+        // Running lecture snippets
+        System.out.println("Running Lecture Examples:");
+        Lecture.runLecture();
+
+        // Running exercises
+        System.out.println("\nRunning Exercises:");
+        com.bootcamp.module01.Exercises.solveExercise1();
+        Exercises.solveExercise2();
+    }
 }
